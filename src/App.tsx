c4a3cf1e5729d55bd40b0bb44e0ac99d44b1644f
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-03-26 00:14:45 +0100

    Add AppBar, Avatar and social media links.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,124 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Grid,
+  IconButton,
+  makeStyles,
+  Avatar,
+  Paper
+} from "@material-ui/core";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaStackOverflow,
+  FaTwitter,
+  FaInstagram
+} from "react-icons/fa";
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    background: theme.palette.primary.dark
+  },
+  main: {
+    height: "75vh",
+    backgroundColor: theme.palette.primary.main
+  },
+  avatar: {
+    width: theme.spacing(6) * 5,
+    height: theme.spacing(6) * 5
+  },
+  name: {
+    padding: theme.spacing(2)
+  },
+  social: {
+    padding: theme.spacing(3),
+    "& > .MuiIconButton-root": {
+      background: theme.palette.primary.dark,
+      margin: theme.spacing(1),
+      "&:hover": {
+        background: theme.palette.secondary.main
+      }
+    }
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppBar position="sticky" className={classes.appBar}>
+        <Toolbar></Toolbar>
+      </AppBar>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        component={Paper}
+        square
+        className={classes.main}
+      >
+        <Grid item>
+          <Avatar
+            alt="Diego Garcia Lozano"
+            src="/static/picture-square.png"
+            className={classes.avatar}
+          />
+        </Grid>
+        <Grid item className={classes.name}>
+          <Typography variant="h4">Diego Garcia Lozano</Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="h5">Software Enginner</Typography>
+        </Grid>
+        <Grid item justify="center" className={classes.social}>
+          <IconButton
+            title="Linkedin"
+            component="a"
+            href="https://www.linkedin.com/in/diegogarcialozano/"
+            target="_blank"
+          >
+            <FaLinkedinIn />
+          </IconButton>
+          <IconButton
+            title="Github"
+            component="a"
+            href="https://github.com/dglozano"
+            target="_blank"
+          >
+            <FaGithub />
+          </IconButton>
+          <IconButton
+            title="StackOverflow"
+            component="a"
+            href="https://stackoverflow.com/users/10648865/dglozano"
+            target="_blank"
+          >
+            <FaStackOverflow />
+          </IconButton>
+          <IconButton
+            title="Twitter"
+            component="a"
+            href="https://twitter.com/dglozano95"
+            target="_blank"
+          >
+            <FaTwitter />
+          </IconButton>
+          <IconButton
+            title="Instagram"
+            component="a"
+            href="https://instagram.com/diegogarcialozano"
+            target="_blank"
+          >
+            <FaInstagram />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
