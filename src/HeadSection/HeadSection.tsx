aae49f1aa3a166fd8c0402977e3b55a0c7b48e46
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-03-28 20:08:35 +0100

    Refactor social media links in HeadSection and center items on narrow resolution

--- src/HeadSection/HeadSection.tsx
+++ src/HeadSection/HeadSection.tsx
@@ -18,27 +18,63 @@ import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles(theme => ({
   main: {
-    height: "75vh",
-    backgroundColor: theme.palette.primary.main
+    minHeight: "75vh",
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(3)
   },
   avatar: {
     width: theme.spacing(6) * 5,
-    height: theme.spacing(6) * 5
+    height: theme.spacing(6) * 5,
+    marginBottom: theme.spacing(2)
   },
-  name: {
-    padding: theme.spacing(2)
+  textPadding: {
+    padding: theme.spacing(1)
   },
-  social: {
-    padding: theme.spacing(3),
-    "& > .MuiIconButton-root": {
-      background: theme.palette.primary.dark,
-      margin: theme.spacing(1),
-      "&:hover": {
-        background: theme.palette.secondary.main
-      }
+  socialMediaContainer: {
+    padding: theme.spacing(3)
+  },
+  socialMediaIconButton: {
+    background: theme.palette.primary.dark,
+    margin: theme.spacing(1),
+    "&:hover": {
+      background: theme.palette.secondary.main
     }
   }
 }));
+
+type SocialMediaLinkDetails = {
+  title: string;
+  href: string;
+  Icon: React.ReactNode;
+};
+
+const socialMediaAccountsLinkDetails: Array<SocialMediaLinkDetails> = [
+  {
+    title: "Linkedin",
+    href: "https://www.linkedin.com/in/diegogarcialozano/",
+    Icon: <FaLinkedinIn />
+  },
+  {
+    title: "Github",
+    href: "https://github.com/dglozano",
+    Icon: <FaGithub />
+  },
+  {
+    title: "StackOverflow",
+    href: "https://stackoverflow.com/users/10648865/dglozano",
+    Icon: <FaStackOverflow />
+  },
+  {
+    title: "Twitter",
+    href: "https://twitter.com/dglozano95",
+    Icon: <FaTwitter />
+  },
+  {
+    title: "Instagram",
+    href: "https://instagram.com/diegogarcialozano",
+    Icon: <FaInstagram />
+  }
+];
 
 const HeadSection = () => {
   const classes = useStyles();
@@ -61,53 +97,36 @@ const HeadSection = () => {
           className={classes.avatar}
         />
       </Grid>
-      <Grid item className={classes.name}>
-        <Typography variant="h4">Diego Garcia Lozano</Typography>
+      <Grid item className={classes.textPadding}>
+        <Typography variant="h4" align="center">
+          Diego Garcia Lozano
+        </Typography>
       </Grid>
-      <Grid item>
-        <Typography variant="h5">{t("softwareEngineer")}</Typography>
+      <Grid item className={classes.textPadding}>
+        <Typography variant="h5" align="center">
+          {t("softwareEngineer")}
+        </Typography>
       </Grid>
-      <Grid item className={classes.social}>
-        <IconButton
-          title="Linkedin"
-          component="a"
-          href="https://www.linkedin.com/in/diegogarcialozano/"
-          target="_blank"
-        >
-          <FaLinkedinIn />
-        </IconButton>
-        <IconButton
-          title="Github"
-          component="a"
-          href="https://github.com/dglozano"
-          target="_blank"
-        >
-          <FaGithub />
-        </IconButton>
-        <IconButton
-          title="StackOverflow"
-          component="a"
-          href="https://stackoverflow.com/users/10648865/dglozano"
-          target="_blank"
-        >
-          <FaStackOverflow />
-        </IconButton>
-        <IconButton
-          title="Twitter"
-          component="a"
-          href="https://twitter.com/dglozano95"
-          target="_blank"
-        >
-          <FaTwitter />
-        </IconButton>
-        <IconButton
-          title="Instagram"
-          component="a"
-          href="https://instagram.com/diegogarcialozano"
-          target="_blank"
-        >
-          <FaInstagram />
-        </IconButton>
+      <Grid
+        item
+        container
+        alignContent="center"
+        alignItems="center"
+        justify="center"
+        className={classes.socialMediaContainer}
+      >
+        {socialMediaAccountsLinkDetails.map(({ title, href, Icon }, index) => (
+          <IconButton
+            title={title}
+            component="a"
+            href={href}
+            target="_blank"
+            key={index}
+            className={classes.socialMediaIconButton}
+          >
+            {Icon}
+          </IconButton>
+        ))}
       </Grid>
     </Grid>
   );
