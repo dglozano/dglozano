00a2b19921ffab05e0400bced53e98a0a9421969
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-03-28 20:08:51 +0100

    Set application minWidth to 320px

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import AppToolbar from "AppToolbar/AppToolbar";
 import HeadSection from "HeadSection/HeadSection";
 
 const App = () => (
-  <>
+  <div style={{ minWidth: 320 }}>
     <AppToolbar />
     <HeadSection />
-  </>
+  </div>
 );
 
 export default App;
