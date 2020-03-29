861807ec42db3fbe1f5dc1710028d5ab063dc4cf
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-03-29 15:20:50 +0200

    Use theme.mixins.toolbar and use flex container for the App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import React from "react";
 
 import AppToolbar from "AppToolbar/AppToolbar";
 import HeadSection from "HeadSection/HeadSection";
+import { Grid, makeStyles } from "@material-ui/core";
 
-const App = () => (
-  <div style={{ minWidth: 320 }}>
-    <AppToolbar />
-    <HeadSection />
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    minWidth: 320,
+    minHeight: "100vh"
+  },
+  content: {
+    flex: 1
+  }
+}));
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container direction="column" className={classes.root}>
+      <AppToolbar />
+      <Grid container direction="column" className={classes.content}>
+        <HeadSection />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default App;
