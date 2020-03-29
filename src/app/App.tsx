3e49239b603b40e621293b36f3c16251eae478fa
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-03-29 21:22:28 +0200

    Fix scroll to the very top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -41,7 +41,7 @@ const App = () => {
     ).querySelector("#back-to-top-anchor");
 
     if (anchor) {
-      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
+      anchor.scrollIntoView({ behavior: "smooth", block: "end" });
     }
   };
 
