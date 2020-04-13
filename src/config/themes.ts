b0cf8a545e51a59dae19618b34a1f2dd4b708138
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2020-04-13 23:31:46 +0200

    Refactor darkTheme into a const rather than a function and add blue colors to palette

--- src/config/themes.ts
+++ src/config/themes.ts
@@ -1,33 +1,61 @@
-import { createMuiTheme, responsiveFontSizes, Theme } from "@material-ui/core";
+import { createMuiTheme, responsiveFontSizes } from "@material-ui/core";
 import { blueGrey, deepOrange } from "@material-ui/core/colors";
+
+declare module "@material-ui/core/styles/createMuiTheme" {
+  interface Theme {
+    dglozano: {
+      palette: {
+        lightBlue: string;
+        darkBlue: string;
+      };
+    };
+  }
+
+  interface ThemeOptions {
+    dglozano?: {
+      palette?: {
+        darkBlue?: string;
+        lightBlue?: string;
+      };
+    };
+  }
+}
 
 export const lightTheme = responsiveFontSizes(
   createMuiTheme({
     palette: {
       primary: { main: blueGrey[900] },
-      secondary: { main: deepOrange[800] }
+      secondary: { main: deepOrange[700] },
     },
     typography: {
-      fontFamily: "'Montserrat', sans-serif"
+      fontFamily: "'Montserrat', sans-serif",
+      body1: {
+        fontSize: 20,
+      },
     },
     overrides: {
       MuiButton: {
         root: {
-          borderRadius: 500
-        }
-      }
-    }
+          borderRadius: 500,
+        },
+      },
+    },
+    dglozano: {
+      palette: {
+        darkBlue: "#011f2cee",
+        lightBlue: "#026492DD",
+      },
+    },
   })
 );
 
-export const darkTheme = ({ palette, ...outerTheme }: Theme): Theme => {
-  const { primary, secondary } = palette;
-  return createMuiTheme({
-    ...outerTheme,
+export const darkTheme = responsiveFontSizes(
+  createMuiTheme({
+    ...lightTheme,
     palette: {
       type: "dark",
-      primary: primary,
-      secondary: secondary
-    }
-  });
-};
+      primary: { main: lightTheme.palette.primary.main },
+      secondary: { main: lightTheme.palette.secondary.main },
+    },
+  })
+);
